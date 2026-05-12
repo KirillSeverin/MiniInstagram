4ee49f64176582002faf5d91d9550cb47da1a8c2
Kirill Severin <kirillseverin2020@gmail.com>
2026-05-12 18:04:04 +0300

    Added a base project

---
+++ MiniInstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniInstagram
+//
+//  Created by Kirill Severin on 12.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
